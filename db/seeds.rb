16aebb740572ffc46707d56051be1e79b7feb9d0
Karen Polanco <polanco.k@gmail.com>
2024-04-19 14:23:21 -0400

    added user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "🌱 Seeding spices..."
+
+User.create(first_name: "James", last_name: "Kirk", username: "ohcaptain", password: "enterprise" )
+User.create(first_name: "Nyota", last_name: "Uhura", username: "comms_officer", password: "talktome")
+
+
+puts "✅ Done seeding!"
