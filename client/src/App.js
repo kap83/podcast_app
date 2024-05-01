03e7808d238ff8d1a4a6714761a44e2a6bbf06d7
Karen Polanco <polanco.k@gmail.com>
2024-04-30 20:40:31 -0400

    added browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+
+      </Routes>
+    </>
   );
 }
 
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,10 +2,11 @@
 //stores the whole state of the app in an immutatable obj tree
 
 import {configureStore} from "@reduxjs/toolkit"
+import userReducer from "../features/userSlice"
 
 
 export const store = configureStore ({
     reducer: {
-        
-    }
+        user: userReducer
+    },
 })
