e19d4cda85c59e569c28af7f1bb573bed2157f56
Karen Polanco <polanco.k@gmail.com>
2024-04-30 20:50:43 -0400

    added login form

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,12 +2,31 @@ import React, {useState} from 'react'
 
 export default function Login() {
 
-
-
+  //add code so user allowing user to see their password or keep it hidden
 
   return (
-    <div>
-      
-    </div>
+    <>
+    <form>
+      <label>
+        USERNAME: 
+        <input 
+          type='text'
+        />
+      </label>
+      <br />
+      <br />
+      <label>
+        PASSWORD: 
+        <input 
+          type='password'
+          placeholder='*********'
+        />
+      </label>
+      <button>SUBMIT</button>
+      <br />
+      <p>Don't have an account? Register <button>here</button></p>
+
+      </form>
+    </>
   )
 }
