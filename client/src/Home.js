98215c6b45830f778638332486c3767afce80876
Karen Polanco <polanco.k@gmail.com>
2024-06-22 14:31:12 -0400

    user profile page started

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import LogoutBtn from './LogoutBtn'
-import {useSelector} from 'react-redux'
+import ProfilePage from './ProfilePage'
 
 export default function Home() {
 
   //child component should include favs etcs. and also a 'you don't have any favorites listed, why not search' message
   //search
 
-  const user = useSelector(state => state.user.user)
-
-  console.log(user)
-
   return (
     <>
       <LogoutBtn />
-       <h3>hello {user.first_name}!</h3> 
+      <ProfilePage />
     </>
   )
 }
---
+++ client/src/ProfilePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useSelector} from 'react-redux'
+
+export default function ProfilePage() {
+
+    const user = useSelector(state => state.user.user)
+
+    //console.log(user)
+
+  return (
+    <>
+        <h3>hello {user.first_name}!</h3>
+        <img src={user.avatar_url} alt='icon' />
+    </>
+  )
+}
