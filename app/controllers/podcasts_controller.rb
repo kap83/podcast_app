06d7f4d27cc777cde567e258f8a65cade4f6f932
Karen Polanco <polanco.k@gmail.com>
2024-08-04 18:54:47 -0400

    adding http::net & itunes api

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,2 +1,24 @@
 class PodcastsController < ApplicationController
+      require 'net/http'
+      require 'uri'
+
+      def search 
+        uri = URI('https://itunes.apple.com/search')
+        params = { media: 'podcast', :term: 'Health & Fitness' }
+        uri.query = URI.encode_www_form(params)
+
+        res = Net::HTTP.get_response(uri)
+        if res.is_a?(Net::HTTPSuccess)
+            render json: res.body
+        else
+            render json: { error: 'Failed to fetch data' }, status: :bad_request
+        end
+
+      end
+    
+
+
 end
+
+
+#uri = URI('https://itunes.apple.com/search?media=podcast&term=Health & Fitness')
