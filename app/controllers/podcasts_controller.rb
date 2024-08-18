e519186d3e63db117cb03a3317298f8305d28f71
Karen Polanco <polanco.k@gmail.com>
2024-08-18 18:58:25 -0400

    added logic to narrow search parameters

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,8 +1,8 @@
 class PodcastsController < ApplicationController
       require 'net/http'
       require 'uri'
-      
-        url = URI('https://itunes.apple.com/search?media=podcast&term=Health&Fitness')
+
+        url = URI('https://itunes.apple.com/search?media=podcast&term=Fitness')
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true if url.scheme == 'https'
   
@@ -11,18 +11,21 @@ class PodcastsController < ApplicationController
   
         if response.is_a?(Net::HTTPSuccess)
           parsed_data = JSON.parse(response.body)
-            
           parsed_data['results'].each do |data|
-          #puts "Processing data: #{data.inspect}"
-            Podcast.create(
-              title: data['collectionName'],
-              primary_genre: data['primaryGenreName'],
-              other_genres: data['genres'].join(', '),
-              track_count: data['trackCount'].to_i,
-              content_rating: data['contentAdvisoryRating'],
-              cover_img: data['artworkUrl600'],
-              apple_url: data['collectionViewUrl']
-            )
+            primary_genre = data['primaryGenreName']
+          
+            #puts "Processing data: #{data.inspect}"
+            if primary_genre == "Health & Fitness" || primary_genre == "Fitness"
+              Podcast.create(
+                title: data['collectionName'],
+                primary_genre: data['primaryGenreName'],
+                other_genres: data['genres'].join(', '),
+                track_count: data['trackCount'].to_i,
+                content_rating: data['contentAdvisoryRating'],
+                cover_img: data['artworkUrl600'],
+                apple_url: data['collectionViewUrl']
+              )
+            end
            end
         end
 
